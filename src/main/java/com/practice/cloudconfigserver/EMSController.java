c69b2734edab4696bdddb5c557e7b3d97db2736f
anurag0708 <anuragpandey.hjp@gmail.com>
2019-06-23 22:01:13 +0530

    Created Employee controller

---
+++ src/main/java/com/practice/cloudconfigserver/EMSController.java
@@ -0,0 +1,23 @@
+package com.practice.cloudconfigserver;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EMSController {
+	
+	@GetMapping("/employees")
+	public List<String> getAllEmployees(){
+		List<String> employees=new ArrayList<>();
+		employees.add("Anurag");
+		employees.add("Abhik");
+		employees.add("Rajeev");
+		employees.add("Rohit");
+		employees.add("Vivek");
+		return employees;
+	}
+
+}
